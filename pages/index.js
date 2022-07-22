1ee93552499573d44a1286f3a8727db43e8b7341
Ahmed Said <86842879+A-said-metwally@users.noreply.github.com>
2022-07-22 10:01:36 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,119 +1,124 @@
-import { useEffect,useState } from 'react'
-import Head from 'next/head'
-import {RefreshIcon ,TrendingUpIcon, TrendingDownIcon} from '@heroicons/react/outline'
-import Box from '../components/Box'
-
-export default function Home() {
-
-  const myArray = [1,2,3,4,5,6,1,2,3,4,5,6]
-  const [NewArray , setNewArray] = useState([])
-  const RandomArray = []
-  const indexs = []
-  
-  
-  let counter = 0
-  const [ValidTries , setValidTries ] = useState(0)
-  const [FaildTries , setFaildTries ] = useState(0)
-
-  let Box1 = 0
-  let Box2 = 0
-
-  const select = (e)=>{
-    if(ValidTries + FaildTries === 6) return alert("pls refresh")
-    if(counter < 2){
-      counter = counter +  1 
-      counter === 1 
-      ? Box1 = e
-      :counter === 2
-      ? Box2 = e
-      : null
-    }else{
-      counter = 1
-       Box1 = 0
-       Box2 = 0
-    }
-
-    if(counter === 2){
-      Box1 === Box2 
-      ? setValidTries(ValidTries + 1) 
-      : setFaildTries(FaildTries + 1)
-    }
-  }
-
-// refresh game
-  const random = ()=>{
-    setValidTries(0) 
-    setFaildTries(0)
-
-    RandomArray.length = 0
-    for(let i = 0; i < 100 ; i++){
-      if (RandomArray.length === 12) break
-      const index = Math.floor(Math.random() * 12)
-      if(indexs.indexOf(index) === -1){
-        RandomArray.push(myArray[index])
-        indexs.push(index)
-      }else{
-        continue
-      }
-    }
-
-    setNewArray(RandomArray)
-
-  }
+import { data } from 'autoprefixer';
+import React,{useState} from 'react'
+import Head from '../head'
 
 
 
-  useEffect(()=>{
-    random()   
-  },[])
-  
+function Main() {
+  const [Data, setData] = useState("");
+  const calcBtns = [];
 
+  [9,8,7,6,5,4,3,2,1,0,".","%"].forEach((element , id) => {
+    calcBtns.push(
+      <button 
+        key={id}
+        value={element}
+        onClick={e =>{setData(Data + e.target.value)}}
+        className="bg-gray-300 p-2 w-full h-full 
+         rounded-md cursor-pointer hover:bg-gray-400 text-xl"
+      >
+        {element}
+      </button>
+    )
+  });
   return (
+    <div className='flex justify-center items-center'>
+        <Head/>
+        <div className=' lg:w-1/4 sm:w-2/4 mt-12'>
+          <div className='mr-1 ml-1 bg-gray-400 text-white text-3xl p-1 h-12'>
+            {Data}
+          </div>
+          {/* clear btns */}
+          <div className='flex space-x-2 p-1 mt-1'>
+          <button
+              onClick={()=>setData(Data.substr(0,Data.length - 1))}
+              className=" p-2 bg-blue-300 w-1/2"
+              >
+              Clear
+            </button>
+            <button
+              onClick={()=>setData("")}
+              className=" p-2 bg-blue-400 w-1/2"
+              >
+              CE
+            </button>
+          </div>
 
-      <div className='flex justify-center'>
-        <Head>
-          <title>Memory Game</title>
-        </Head>
+          <div className='flex '>
+            {/* left calc btns */}
+            <div className='flex flex-wrap mt-1'>
+              {calcBtns.map((btn , id)=>{
+                return (
+                  <div 
+                    key={id}
+                    className="  p-1 w-1/3"
+                  >
+                    {btn}
+                  </div>
+                )
+              })}
+            </div>
 
-        <div className='flex flex-col lg:w-2/4 md:w-3/4 sm:w-full h-screen '> 
-          <div className='p-1 w-full' >
-            <div className='flex p-1 justify-between items-center border-4 border-blue-400'>
-              <h1 className='font-bold text-blue-600 text-lg'>Hello : Ahmed</h1>
-              <div className='flex space-x-6'>
-                <div className='flex space-x-12 mr-6'>
-                  <h1 className=' font-bold text-lg text-green-500'>Valid Tries : {ValidTries}</h1>
-                  <h1 className=' font-bold text-lg text-red-500'>Wrong Tries : {FaildTries}</h1>
-                </div>
-                  <RefreshIcon 
-                    onClick={random}
-                    className="h-8 w-8 text-blue-600 hover:rotate-180 transition ease-in-out duration-500 cursor-pointer
-                    hover:text-green-500  "                
-                  /> 
-                
+            {/* right control btns */}
+            <div className='flex flex-col p-1 '>
+            <div className=' flex justify-center items-center p-1 h-1/5 '>
+                <button 
+                  value="/"
+                  onClick={e =>setData(Data + e.target.value)}
+                  className=' flex justify-center items-center p-3
+                   bg-gray-500 h-full w-20 text-teal-200 text-2xl'
+                   >/</button>
+              </div>
+              <div className=' flex justify-center items-center p-1 h-1/5 '>
+                <button 
+                  value="*"
+                  onClick={e =>setData(Data + e.target.value)}
+                  className=' flex justify-center items-center p-3
+                   bg-gray-500 h-full w-20 text-teal-200 text-2xl'
+                   >*</button>
+              </div>
+              <div className=' flex justify-center items-center p-1 h-1/5 '>
+                <button 
+                  value="-"
+                  onClick={e =>setData(Data + e.target.value)}
+                  className=' flex justify-center items-center p-3
+                   bg-gray-500 h-full w-20 text-teal-200 text-2xl'
+                   >-</button>
+              </div>
+              <div className=' flex justify-center items-center p-1 h-1/5 '>
+                <button 
+                  value="+"
+                  onClick={e =>setData(Data + e.target.value)}
+                  className=' flex justify-center items-center p-3
+                   bg-gray-500 h-full w-20 text-teal-200 text-2xl'
+                   >+</button>
+              </div>
+              <div className=' flex justify-center items-center p-1 h-1/5 '>
+                <button 
+                  value="="
+                  onClick={e =>{
+                    try{
+                      setData(
+                        String(eval(Data).length > 3 &&
+                          String(eval(Data).includes("."))
+                            ? String(eval(Data).toFixed(4))
+                              :String(eval(Data))
+                        )
+                      )
+                    }
+                    catch(err){
+                      console.log(err)
+                    }
+                  }}
+                  className=' flex justify-center items-center p-3
+                   bg-gray-500 h-full w-20 text-teal-200 text-2xl'
+                   >=</button>
               </div>
             </div>
           </div>
-          
-          <div className=' flex flex-wrap justify-center mt-5'>
-            {NewArray.map((el ,id)=>{
-              return (
-                <Box el={el} key={id} fun={select} array={NewArray}  />
-              )
-            })}
-          </div>
-
-            {ValidTries + FaildTries === 6 && 
-              <div className=' absolute top-0 left-0 w-screen h-screen bg-gray-100
-                flex items-center justify-center' onClick={random}>
-               { ValidTries > 3 
-                ? <TrendingUpIcon className='h-56 w-56 text-green-400 animate-pulse duration-150'/>
-                : <TrendingDownIcon className='h-56 w-56 text-red-400 animate-pulse duration-150'/>
-                }
-              </div>
-            } 
-
-          </div>
-      </div>
+        </div>
+    </div>
   )
 }
 
+export default Main
